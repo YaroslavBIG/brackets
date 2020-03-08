610198f3f1a18c060a9028418e8a7fa5c94c5f12
FireGUN <yaroslavbig@ya.ru>
2020-03-08 14:15:50 +0200

    feet: test pass; exept 10,13,14

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const openBrackets = []
+    const closeBrackets = []
+    for (const arr of bracketsConfig) {
+        openBrackets.push(arr[0]);
+        closeBrackets.push(arr[1]);
+    }
+    //console.log(str)
+    const strToArr = str.split('');
+    const stack = [];
+    for (const brackets of strToArr) {
+        //console.log(brackets)
+        if (openBrackets.includes(brackets)) {
+            stack.push(openBrackets.indexOf(brackets))
+        }
+        if (closeBrackets.includes(brackets)) {
+                if (closeBrackets.indexOf(brackets) === stack.pop()) {
+                } else {
+                    return false;
+                }
+        }
+    };
+    const result = stack.length == 0;
+    //console.log(openBrackets, closeBrackets)
+    return result;
 }
